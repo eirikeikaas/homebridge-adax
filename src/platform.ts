ccba13debcda8f81a87e2b9974e77f81d1bd513b
Eirik Eikås <eirik.eikas@hyper.no>
2020-10-31 10:13:21 +0100

    Implementation

--- src/platform.ts
+++ src/platform.ts
@@ -1,119 +1,82 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
-import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import fetch from 'node-fetch';
 
-/**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
- */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import { ADAXPlatformAccessory } from './platformAccessory';
+
+export class ADAXHomebridgePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
-  // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
+
+  public token = '';
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
+    this.log.debug('Finished initializing platform:', this.config.name, 'conf', this.config);
 
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      
+      fetch('https://api-1.adax.no/client-api/auth/token', {
+        method: 'POST',
+        body: `grant_type=password&username=${this.config.clientId}&password=${this.config.secret}`,
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+      }).then((res) => {
+        return res.json();
+      }).then((json) => {
+        this.token = json.access_token;
+        
+        this.discoverDevices();
+      });
     });
   }
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to setup event handlers for characteristics and update respective values.
-   */
+
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
 
-    // add the restored accessory to the accessories cache so we can track if it has already been registered
     this.accessories.push(accessory);
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
-  discoverDevices() {
-
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
+  async discoverDevices() {
+    const home = await fetch('https://api-1.adax.no/client-api/rest/v1/content', {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
       },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+    }).then((res) => {
+      return res.json();
+    });
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    for (const device of home.rooms) {
+      const uuid = this.api.hap.uuid.generate(`${device.id}`);
 
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
-
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
       const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
       if (existingAccessory) {
-        // the accessory already exists
         if (device) {
           this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-
-          // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-          // existingAccessory.context.device = device;
-          // this.api.updatePlatformAccessories([existingAccessory]);
-
-          // create the accessory handler for the restored accessory
-          // this is imported from `platformAccessory.ts`
-          new ExamplePlatformAccessory(this, existingAccessory);
+          new ADAXPlatformAccessory(this, existingAccessory);
           
-          // update accessory cache with any changes to the accessory details and information
           this.api.updatePlatformAccessories([existingAccessory]);
         } else if (!device) {
-          // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-          // remove platform accessories when no longer present
           this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
           this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
         }
       } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.name);
+        const accessory = new this.api.platformAccessory(device.name, uuid);
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
         accessory.context.device = device;
+        new ADAXPlatformAccessory(this, accessory);
 
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
     }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,151 +1,162 @@
-import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
+import { Service, PlatformAccessory } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import fetch from 'node-fetch';
+import moment from 'moment';
 
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class ExamplePlatformAccessory {
+import { ADAXHomebridgePlatform } from './platform';
+
+export class ADAXPlatformAccessory {
   private service: Service;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+
+  private roomState = {
+    stamp: moment(),
+    room: {
+      id: null,
+      heatingEnabled: true,
+      targetTemperature: 1800,
+      temperature: 1800,
+    },
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: ADAXHomebridgePlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
-    // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'ADAX')
+      .setCharacteristic(this.platform.Characteristic.Model, 'N/A')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'N/A');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service = this.accessory.getService(this.platform.Service.Thermostat) ||
+                   this.accessory.addService(this.platform.Service.Thermostat);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentHeatingCoolingState)
+      .on('get', this.handleCurrentHeatingCoolingStateGet.bind(this));
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    this.service.getCharacteristic(this.platform.Characteristic.TargetHeatingCoolingState)
+      .on('get', this.handleTargetHeatingCoolingStateGet.bind(this))
+      .on('set', this.handleTargetHeatingCoolingStateSet.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    this.service.getCharacteristic(this.platform.Characteristic.TemperatureDisplayUnits)
+      .on('get', this.handleTemperatureDisplayUnitsGet.bind(this));
+    
+    this.service.getCharacteristic(this.platform.Characteristic.CurrentTemperature)
+      .on('get', this.handleCurrentTemperatureGet.bind(this));
 
+    this.service.getCharacteristic(this.platform.Characteristic.TargetTemperature)
+      .on('get', this.handleTargetTemperatureGet.bind(this))
+      .on('set', this.handleTargetTemperatureSet.bind(this));
 
-    /**
-     * Creating multiple services of the same type.
-     * 
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     * 
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     * 
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     * 
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
+    this.roomState.room = accessory.context.device;
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+  getDeviceStatus() {
+    if(this.roomState.room.id !== undefined && moment(this.roomState.stamp).add(30, 's').isAfter(moment())) {
+      return Promise.resolve(this.roomState.room);
+    }
 
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+    return fetch('https://api-1.adax.no/client-api/rest/v1/content', {
+      headers: {
+        Authorization: `Bearer ${this.platform.token}`,
+      },
+    }).then((res) => {
+      if (res.status === 429) {
+        return Promise.resolve({ rooms: [this.roomState.room] });
+      }
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+      return res.json();
+    }).then((json) => {
+      const room = json.rooms.filter((e) => {
+        return e.id === this.accessory.context.device.id;
+      })[0];
 
-    // you must call the callback function
-    callback(null);
+      this.roomState = {
+        stamp: moment(),
+        room: room,
+      };
+
+      return room;
+    }).catch(err => console.log(err));
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  getOn(callback: CharacteristicGetCallback) {
-
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
-    callback(null, isOn);
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
-
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-
-    // you must call the callback function
-    callback(null);
+  heatingState() {
+    const { targetTemperature, temperature } = this.roomState.room;
+    return targetTemperature > temperature ? 1 : 2;
   }
 
+  _setDeviceStatus(state, delay = 0) {
+    return this.sleep(delay).then(() => {
+      return fetch('https://api-1.adax.no/client-api/rest/v1/control', {
+        method: 'POST',
+        body: JSON.stringify({
+          rooms: [
+            {
+              id: this.accessory.context.device.id,
+              ...state,
+            },
+          ],
+        }),
+        headers: {
+          Authorization: `Bearer ${this.platform.token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+    });
+  }
+
+  setDeviceStatus(state, delay = 0) {
+    return this._setDeviceStatus(state, delay).then((res) => {
+      if(res.status === 429) {
+        return this._setDeviceStatus(state, 5000);
+      } else {
+        return Promise.resolve(res);
+      }
+    }).then((res) => {
+      return res.text();
+    });
+  }
+
+  handleCurrentHeatingCoolingStateGet(callback) {
+    callback(null, this.heatingState());
+  }
+
+  handleTargetHeatingCoolingStateGet(callback) {
+    callback(null, this.heatingState());
+  }
+
+  handleTargetHeatingCoolingStateSet(value, callback) {
+    callback(null, this.heatingState());
+  }
+
+  handleCurrentTemperatureGet(callback) {
+    this.getDeviceStatus().then((state) => {
+      callback(null, (state.temperature/100).toFixed(1));
+    });
+  }
+
+  handleTemperatureDisplayUnitsGet(callback) {
+    callback(this.platform.Characteristic.TemperatureDisplayUnits.CELSIUS);
+  }
+
+  handleTargetTemperatureGet(callback) {
+    this.getDeviceStatus().then((state) => {
+      callback(null, (state.targetTemperature/100).toFixed(1));
+    });
+  }
+
+  handleTargetTemperatureSet(value, callback) {
+    this.setDeviceStatus({
+      targetTemperature: value*100,
+    }).then(() => {
+      callback(null);
+    });
+  }
 }
